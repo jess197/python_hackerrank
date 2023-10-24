4e51af819a76b3fde673817011b040112b2fa7ac
Jess <jessicacostaesilva97@gmail.com>
2023-10-23 21:40:40 -0300

    sum and mean exercise

---
+++ sum_and_mean.py
@@ -0,0 +1,12 @@
+'''
+Please write a program which asks the user for four numbers. 
+The program then prints out the sum and the mean of the numbers. 
+'''
+
+# Write your solution here
+number1 = int(input("A number? "))
+number2 = int(input("A second number? "))
+number3 = int(input("A third number? "))
+number4 = int(input("A forth number? "))
+sum = number1+number2+number3+number4
+print(f"The sum of the numbers is {sum} and the means is {sum/4}")
