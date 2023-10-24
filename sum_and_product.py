39e7f09745553c03338cea6289f65e763af9bad1
Jess <jessicacostaesilva97@gmail.com>
2023-10-23 21:35:36 -0300

    sum and product exercise

---
+++ sum_and_product.py
@@ -0,0 +1,10 @@
+'''
+Please write a program which asks the user for two numbers. 
+The program will then print out the sum and the product of the two numbers.
+'''
+
+# Write your solution here
+number1 = int(input("A number? "))
+number2 = int(input("A second number? "))
+print(f"The sum of the numbers: {number1+number2}")
+print(f"The product of the numbers: {number1*number2}")
